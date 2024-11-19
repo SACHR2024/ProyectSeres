1dde4b3701754bdf25fc1900063fa610b13a93d6
Samuel Chirinos <samuelchirinos71@gmail.com>
2024-11-19 17:59:38 -0500

    pantalla principal de usuarios logeados agregada

--- functions/login.php
+++ functions/login.php
@@ -11,8 +11,9 @@
 
     if ($resp) {
         echo "<h1>Bienvenido!!</h1>";
+        header("refresh:2; url=http://localhost/proyectSeres/templates/logged/userHome.html");
     }else {
         echo "<h1>FFFFFFF</h1>";
-            
-        header("refresh:2; url=http://localhost/LOGINLEARNING/");
+        header("refresh:2; url=http://localhost/proyectSeres/templates/login.html");
+        
     }
